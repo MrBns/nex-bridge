0d7f7316f50380d0206ce2c77f05c376e22a307c
iamajraj <akmalraj07@gmail.com>
2024-02-14 22:50:17 +0600

    ReadyToTake section is done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import Image from 'next/image';
+import ReadyToTake from '../components/ReadyToTake';
 
 export default function Home() {
-  return <main className="bg-black">
-
-  </main>;
+  return (
+    <main className="bg-black text-white flex flex-col w-full h-full">
+      <ReadyToTake />
+    </main>
+  );
 }
---
+++ src/components/ReadyToTake.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ArrowIcon from '../assets/ArrowIcon';
+
+type Props = {};
+
+function ReadyToTake({}: Props) {
+  return (
+    <div className="my-container mx-auto flex justify-between gap-10 mt-10">
+      <div className="w-[75%] rounded-[50px] flex items-end justify-between pt-20 pb-10 pl-16 pr-28 bg-[url('/ready-to-take-bg.svg')] bg-cover bg-no-repeat bg-bottom">
+        <p className="text-[70.37px] leading-[75.7px] font-medium">
+          We envision a future
+          <br />
+          where
+          <br />
+          traditional finance
+          <br />
+          integrates
+          <br />
+          seamlessly with
+          <br />
+          blockchain
+        </p>
+        <button className="bg-white text-[#1C75BC] px-8 py-6 rounded-[15px] mb-10 flex items-center gap-3">
+          Contact
+          <ArrowIcon />
+        </button>
+      </div>
+      <div className="flex flex-col justify-between">
+        <div className="flex flex-col justify-start gap-1">
+          <p className="text-[100px] leading-[110px] font-medium">500+</p>
+          <span className="text-[32px] leading-[40px]">
+            Successful Projects
+          </span>
+        </div>
+        <div className="flex flex-col justify-start gap-1">
+          <p className="text-[100px] leading-[110px] font-medium">98%</p>
+          <span className="text-[32px] leading-[40px]">Satisfied Clients</span>
+        </div>
+        <div className="flex flex-col justify-start gap-1">
+          <p className="text-[100px] leading-[110px] font-medium">500+</p>
+          <span className="text-[32px] leading-[40px]">Handled Countries</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ReadyToTake;
