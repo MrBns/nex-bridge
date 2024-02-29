73325816f0dde57475683705005023565b300937
iamajraj <akmalraj07@gmail.com>
2024-02-29 16:32:03 +0600

    Swiper slide animation added to section of why choose us

--- src/views/homepage/SectionWhyChooseUs.tsx
+++ src/views/homepage/SectionWhyChooseUs.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   IMG_ILLST_INDUSTRY_LEADER,
   IMG_ILLST_REGISTER_ISSUER,
@@ -6,8 +8,34 @@ import {
   IMG_WHY_CHOOSE_US_WAVE,
 } from "@/assets/img";
 import Img from "@/components/helper/Img";
+import { Autoplay } from "swiper/modules";
+import { SwiperSlide, Swiper } from "swiper/react";
 
 const WHY_CHOOSE_US_CARD_DATA = [
+  {
+    id: "data-1",
+    img: IMG_ILLST_REGISTER_ISSUER.src,
+    title: "Registerd Issuer",
+    desc: "NexBridge is registered as a digital asset issuer in the El Salvador National Commission of Digital Assets registry, holding the license number EAD-0003",
+  },
+  {
+    id: "data-1",
+    img: IMG_ILLST_INDUSTRY_LEADER.src,
+    title: "Industry Leader",
+    desc: "Created with the aim of being an industry leader in issuing digital assets in regulated markets",
+  },
+  {
+    id: "data-1",
+    img: IMG_LILLST_FINANCIAL_KNOW_HOW.src,
+    title: "Financial Know-how",
+    desc: "Our founding members and stakeholders have longstanding experience in traditional finance and hold licenses across multiple countries",
+  },
+  {
+    id: "data-1",
+    img: IMG_ILLUST_GLOBAL_PRESENCE.src,
+    title: "Global presence",
+    desc: "Group of companies consolidated in jurisdictions globally accepted as financial centers, partnership with industry leader across different sectors",
+  },
   {
     id: "data-1",
     img: IMG_ILLST_REGISTER_ISSUER.src,
@@ -53,24 +81,56 @@ export default function SectionWhyChooseUs() {
         </div>
 
         <div className="">
-          <div className="flex flex-wrap">
-            {WHY_CHOOSE_US_CARD_DATA.map((d, idx) => (
-              <div
-                key={idx + d.id}
-                data-aos
-                className="w-1/2 lg:w-3/12 group rounded-3xl p-2 lg:p-7 hover:scale-105 transition-all duration-500"
-              >
-                <Img
-                  src={d.img}
-                  alt={d.title}
-                  className="mb-8 h-[100px] opacity-60 group-hover:opacity-100 transition-all duration-500 group-hover:scale-110 group-hover:rotate-[20deg]"
-                />
-                <div className="">
-                  <h1 className="lg:text-3xl mb-4 font-semibold">{d.title}</h1>
-                  <p className="text-xs">{d.desc}</p>
-                </div>
-              </div>
-            ))}
+          <div className="relative flex flex-wrap why-choose-us-slider slider-shadow">
+            <Swiper
+              modules={[Autoplay]}
+              breakpoints={{
+                0: {
+                  slidesPerView: 1,
+                },
+                734: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                1024: {
+                  slidesPerView: 3,
+                },
+                1400: {
+                  slidesPerView: 4,
+                },
+              }}
+              allowTouchMove={true}
+              freeMode={true}
+              loop={true}
+              autoplay={{
+                disableOnInteraction: false,
+                delay: 0,
+              }}
+              speed={12000}
+              direction="horizontal"
+            >
+              {WHY_CHOOSE_US_CARD_DATA.map((d, idx) => (
+                <SwiperSlide key={idx + d.id} className="w-full">
+                  <div
+                    data-aos
+                    // w-1/2 lg:w-3/12
+                    className="group rounded-3xl p-2 lg:p-7 hover:scale-105 transition-all duration-500"
+                  >
+                    <Img
+                      src={d.img}
+                      alt={d.title}
+                      className="mb-8 h-[100px] opacity-60 group-hover:opacity-100 transition-all duration-500 group-hover:scale-110 group-hover:rotate-[20deg]"
+                    />
+                    <div className="">
+                      <h1 className="lg:text-3xl mb-4 font-semibold">
+                        {d.title}
+                      </h1>
+                      <p className="text-xs">{d.desc}</p>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
         </div>
       </div>
