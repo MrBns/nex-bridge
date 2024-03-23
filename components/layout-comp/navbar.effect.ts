6aca4f91f3f95a9b2d4db017fa99ea7029c044e9
MrBns <hi@MrBns.dev>
2024-03-23 14:35:44 +0600

    changed navbar effect style

--- components/layout-comp/navbar.effect.ts
+++ components/layout-comp/navbar.effect.ts
@@ -1,38 +1,31 @@
 "use client";
 import isDomReady from "@/lib/client/isDomReady";
 
-isDomReady()
-  .interactive()
-  .then(() => {
-    console.log("Dom is Ready");
-    const navbar_el: HTMLElement = document.getElementById("main-navbar")!;
-    const scrolled_class = "navbar-scrolled";
+window.addEventListener("load", () => {
+  console.log("Dom is Ready");
+  const navbar_el: HTMLElement = document.getElementById("main-navbar")!;
+  const scrolled_class = "navbar-scrolled";
 
-    function handle_scrolling() {
-      if (window.scrollY > 1) {
-        navbar_el?.classList.add(scrolled_class);
-      } else {
-        navbar_el?.classList.remove(scrolled_class);
-      }
+  function handle_scrolling() {
+    if (window.scrollY > 1) {
+      navbar_el?.classList.add(scrolled_class);
+    } else {
+      navbar_el?.classList.remove(scrolled_class);
     }
+  }
 
-    window.addEventListener("scroll", (e) => {
-      handle_scrolling();
-    });
+  window.addEventListener("scroll", (e) => {
     handle_scrolling();
-  })
-  .catch((e) => {});
+  });
+  handle_scrolling();
+});
 
 isDomReady()
   .completed()
   .then(() => {
     const NAVBAR_VISIBLE_CLASS = "navbar-content-show";
-    const navbar_toggle_button = document.getElementById(
-      "navbar-toggle-button"
-    );
-    const navbar_menu_close_button = document.getElementById(
-      "navbar-menu-close-button"
-    );
+    const navbar_toggle_button = document.getElementById("navbar-toggle-button");
+    const navbar_menu_close_button = document.getElementById("navbar-menu-close-button");
     const navbar_menu_el = document.getElementById("navbar-menu-el");
 
     //showing content;
@@ -40,9 +33,7 @@ isDomReady()
       navbar_menu_el?.classList.add(NAVBAR_VISIBLE_CLASS);
     });
 
-    const navbar_menu_links = document.querySelectorAll(
-      "#main-navbar .navbar-menu-link"
-    );
+    const navbar_menu_links = document.querySelectorAll("#main-navbar .navbar-menu-link");
 
     //hiding menu on each link clicks;
     navbar_menu_links.forEach((link) => {
